6522991ed8e3a0910e295178040c9d6f0075c04c
macmade <macmade>
2011-01-31 19:42:43 +0000

    Added the process information getter class.

---
+++ ProcessRenicer/Classes/ProcessInfos.m
@@ -0,0 +1,289 @@
+/*******************************************************************************
+ * Copyright (c) 2011, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+/*!
+ * @file        ProcessInfos.m
+ * @copyright   eosgarden 2011 - Jean-David Gadina <macmade@eosgarden.com>
+ * @abstract    ...
+ */
+
+#import "ProcessInfos.h"
+#import "Process.h"
+
+@interface ProcessInfos( Private )
+
+- ( void )getProcesses;
+
+@end
+
+@implementation ProcessInfos( Private )
+
+- ( void )getProcesses
+{
+    NSTask              * task;
+    NSPipe              * pipe;
+    NSFileHandle        * fh;
+    NSData              * data;
+    NSString            * output;
+    NSMutableArray      * lines;
+    NSString            * line;
+    NSString            * command;
+    NSAutoreleasePool   * ap;
+    NSMutableDictionary * threadDict;
+    NSMutableDictionary * processDict;
+    Process             * process;
+    NSUInteger            pid;
+    BOOL                  exitThread;
+    
+    ap         = [ NSAutoreleasePool new ];
+    threadDict = [ [ NSThread currentThread ] threadDictionary ];
+    exitThread = NO;
+    
+    [ threadDict setValue: [ NSNumber numberWithBool: exitThread ] forKey: @"ProcessesThreadShouldExit" ];
+    
+    while( exitThread == NO )
+    {
+        if( ap == nil )
+        {
+            ap = [ NSAutoreleasePool new ];
+        }
+        
+        running = YES;
+        
+        [ processes release ];
+        
+        processes   = [ [ NSMutableDictionary dictionaryWithCapacity: 100 ] retain ];
+        processDict = [ NSMutableDictionary dictionaryWithCapacity: 100 ];
+        
+        /***********************************************************************
+         * 
+         **********************************************************************/
+        
+        task = [ NSTask new ];
+        pipe = [ NSPipe pipe ];
+        
+        [ task setLaunchPath: @"/bin/ps" ];
+        [ task setArguments: [ NSArray arrayWithObjects: @"-ec", @"-o", @"pid,command", nil ] ];
+        [ task setStandardOutput: pipe ];
+        [ task launch ];
+        
+        while( [ task isRunning ] == YES )
+        {
+            usleep( 10 );
+        }
+        
+        fh     = [ pipe fileHandleForReading ];
+        data   = [ fh readDataToEndOfFile ];
+        output = [ [ NSString alloc ] initWithData: data encoding: NSASCIIStringEncoding ];
+        lines  = [ NSMutableArray arrayWithArray: [ output componentsSeparatedByString: @"\n" ] ];
+        
+        [ lines removeObjectAtIndex: 0 ];
+        
+        for( line in lines )
+        {
+            @try
+            {
+                pid     = [ [ line substringToIndex: 5 ] integerValue ];
+                command = [ line substringFromIndex: 6 ];
+            }
+            @catch( NSException * e )
+            {
+                continue;
+            }
+            
+            if( [ command isEqualToString: @"ps" ] )
+            {
+                continue;
+            }
+            
+            process = [ Process processWithPid: pid name: command ];
+            
+            [ processDict setObject: process forKey: [ NSString stringWithFormat: @"%06u", ( unsigned int )pid ] ];
+        }
+        
+        [ output release ];
+        [ task release ];
+        
+        /***********************************************************************
+         * 
+         **********************************************************************/
+        
+        task = [ NSTask new ];
+        pipe = [ NSPipe pipe ];
+        
+        [ task setLaunchPath: @"/bin/ps" ];
+        [ task setArguments: [ NSArray arrayWithObjects: @"-e", @"-o", @"pid,command", nil ] ];
+        [ task setStandardOutput: pipe ];
+        [ task launch ];
+        
+        while( [ task isRunning ] == YES )
+        {
+            usleep( 10 );
+        }
+        
+        fh     = [ pipe fileHandleForReading ];
+        data   = [ fh readDataToEndOfFile ];
+        output = [ [ NSString alloc ] initWithData: data encoding: NSASCIIStringEncoding ];
+        lines  = [ NSMutableArray arrayWithArray: [ output componentsSeparatedByString: @"\n" ] ];
+        
+        [ lines removeObjectAtIndex: 0 ];
+        
+        for( line in lines )
+        {
+            @try
+            {
+                pid     = [ [ line substringToIndex: 5 ] integerValue ];
+                command = [ line substringFromIndex: 6 ];
+            }
+            @catch( NSException * e )
+            {
+                continue;
+            }
+            
+            if( [ command isEqualToString: @"ps -e -p pid,command" ] )
+            {
+                continue;
+            }
+            
+            process = [ processDict objectForKey: [ NSString stringWithFormat: @"%06u", ( unsigned int )pid ] ];
+            
+            if( process == nil )
+            {
+                continue;
+            }
+            
+            process.command = command;
+        }
+        
+        [ output release ];
+        [ task release ];
+        
+        /***********************************************************************
+         * 
+         **********************************************************************/
+        
+        task = [ NSTask new ];
+        pipe = [ NSPipe pipe ];
+        
+        [ task setLaunchPath: @"/bin/ps" ];
+        [ task setArguments: [ NSArray arrayWithObjects: @"-ax", @"-o", @"pid,ppid,uid,gid,nice,pri,paddr,rss,vsz,%cpu,%mem,lstart,user", nil ] ];
+        [ task setStandardOutput: pipe ];
+        [ task launch ];
+        
+        while( [ task isRunning ] == YES )
+        {
+            usleep( 10 );
+        }
+        
+        fh     = [ pipe fileHandleForReading ];
+        data   = [ fh readDataToEndOfFile ];
+        output = [ [ NSString alloc ] initWithData: data encoding: NSASCIIStringEncoding ];
+        lines  = [ NSMutableArray arrayWithArray: [ output componentsSeparatedByString: @"\n" ] ];
+        
+        [ lines removeObjectAtIndex: 0 ];
+        
+        for( line in lines )
+        {
+            @try
+            {
+                pid = [ [ line substringToIndex: 5 ] integerValue ];
+            }
+            @catch( NSException * e )
+            {
+                continue;
+            }
+            
+            process = [ processDict objectForKey: [ NSString stringWithFormat: @"%06u", ( unsigned int )pid ] ];
+            
+            if( process == nil )
+            {
+                continue;
+            }
+            
+            @try
+            {
+                
+                /*
+   10     1     0     0  0  33  d7f52a0   6868  2451588   0.0  0.1 Fri Jan 28 10:19:05 2011     root    
+
+                */
+                
+                process.ppid  = [ [ [ line substringFromIndex: 6  ] substringToIndex: 5 ] integerValue ];
+                process.uid   = [ [ [ line substringFromIndex: 12 ] substringToIndex: 5 ] integerValue ];
+                process.gid   = [ [ [ line substringFromIndex: 18 ] substringToIndex: 5 ] integerValue ];
+                process.nice  = [ [ [ line substringFromIndex: 24 ] substringToIndex: 2 ] integerValue ];
+                process.pri   = [ [ [ line substringFromIndex: 27 ] substringToIndex: 3 ] integerValue ];
+                process.paddr = [ NSString stringWithFormat: @"0x%@", [ [ [ line substringFromIndex: 32 ] substringToIndex: 7 ] uppercaseString ] ];
+            }
+            @catch( NSException * e )
+            {
+                continue;
+            }
+            
+            [ processes setObject: process forKey: [ NSString stringWithFormat: @"%06u", ( unsigned int )pid ] ];
+        }
+        
+        running = NO;
+        
+        exitThread = [ [ threadDict valueForKey: @"ASLThreadShouldExit" ] boolValue ];
+        
+        if( exitThread == NO )
+        {
+            [ NSThread sleepForTimeInterval: 5 ];
+        }
+        
+        [ output release ];
+        [ task release ];
+        [ ap release ];
+        
+        ap = nil;
+    }
+}
+
+@end
+
+@implementation ProcessInfos
+
+@synthesize running;
+
+- ( id )init
+{
+    if( ( self = [ super init ] ) )
+    {
+        [ self performSelectorInBackground: @selector( getProcesses ) withObject: nil ];
+    }
+    
+    return self;
+}
+
+- ( void )dealloc
+{
+    [ processes release ];
+    [ super dealloc ];
+}
+
+- ( NSArray * )processes
+{
+    NSArray * values;
+    
+    while( self.isRunning == YES );
+    
+    values = [ [ processes allValues ] sortedArrayUsingComparator: ^( Process * obj1, Process * obj2 )
+        {
+            if( obj1.pid > obj2.pid )
+            {
+                return ( NSComparisonResult )NSOrderedAscending;
+            }
+            
+            return ( NSComparisonResult )NSOrderedDescending;
+        }
+    ];
+    
+    return values;
+}
+
+@end
---
+++ ProcessRenicer/Classes/include/ProcessInfos.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2011, Jean-David Gadina <macmade@eosgarden.com>
+ * All rights reserved
+ ******************************************************************************/
+ 
+/* $Id$ */
+
+/*!
+ * @header      ProcessInfos.h
+ * @copyright   eosgarden 2011 - Jean-David Gadina <macmade@eosgarden.com>
+ * @abstract    ...
+ */
+
+/*!
+ * @class       ProcessInfos
+ * @abstract    ...
+ */
+@interface ProcessInfos: NSObject
+{
+@protected
+    
+    NSMutableDictionary * processes;
+    BOOL                  running;
+    
+@private
+    
+    id r1;
+    id r2;
+}
+
+@property( readonly ) NSArray * processes;
+@property( readonly, getter = isRunning ) BOOL running;
+
+@end
